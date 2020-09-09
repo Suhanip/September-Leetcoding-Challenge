6d26f3c26e94e47c65941d724c7fc0d7c9ce09ae
Suhanip <47884038+Suhanip@users.noreply.github.com>
2020-09-09 11:20:13 +0530

    Sum of root to binary numbers

---
+++ day_08.cpp
@@ -0,0 +1,44 @@
+/*PROBLEM STATEMENT:
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+Example 1:
+             1
+          /    \
+         0      1
+       /   \   /  \
+      0     1 0     1
+      
+Input: [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+*/
+
+//CODE:
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int sumRootToLeaf(TreeNode* root,int sum){
+        if(!root) return 0;
+        sum = (sum << 1) + root->val;
+        //checks if the node is leaf or not
+        if(!root->left && !root->right) return sum;
+        return sumRootToLeaf(root->left, sum) + sumRootToLeaf(root->right, sum);
+    }
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return sumRootToLeaf(root,0);
+    }
+};
